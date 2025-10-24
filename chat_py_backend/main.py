d3e7e17545c2fbe3b7e762c5f1f0b84ef95b62bb
dede-08 <benandrealanyaguevara@gmail.com>
2025-10-23 19:52:06 -0500

    updates

--- chat_py_backend/main.py
+++ chat_py_backend/main.py
@@ -13,9 +13,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
